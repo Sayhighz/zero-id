7405ae262d066e4372a0a3528e97445cadea6191
Pratan Nilson <119069194+Sayhighz@users.noreply.github.com>
2026-01-22 21:05:17 +0700

    setup(app): add template app

---
+++ android/app/src/main/java/com/zero/id/app/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.zero.id.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.zero.id.app.databinding.ActivityMainBinding
+import com.zero.id.app.R
+import com.zero.id.library.FactorialCalculator
+import com.zero.id.library.android.ToastUtil
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCompute.setOnClickListener {
+            val message = if (binding.editTextFactorial.text.isNotEmpty()) {
+                val input = binding.editTextFactorial.text.toString().toLong()
+                val result = try {
+                    FactorialCalculator.computeFactorial(input).toString()
+                } catch (ex: IllegalStateException) {
+                    "Error: ${ex.message}"
+                }
+
+                binding.textResult.text = result
+                binding.textResult.visibility = View.VISIBLE
+                getString(R.string.notification_title, result)
+            } else {
+                getString(R.string.please_enter_a_number)
+            }
+            ToastUtil.showToast(this, message)
+        }
+
+        binding.buttonAppcompose.setOnClickListener {
+            val intent = Intent(it.context, ComposeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
